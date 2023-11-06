f59b7c7e9f8852403bbfb7d7939fa8f21865e572
azazo1 <azazo1@qq.com>
2023-11-06 15:13:08 +0800

    修改了单片机连接服务器IP

--- src/com/azazo1/dormtransferfile/SCMConnector.java
+++ src/com/azazo1/dormtransferfile/SCMConnector.java
@@ -11,7 +11,7 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public final class SCMConnector implements Closeable {
-    public static final String SCM_IP = "192.168.1.108";
+    public static final String SCM_IP = "192.168.1.104";
     public static final int SCM_PORT = 8088;
     private final Socket socket;
     public final InputStream in;
